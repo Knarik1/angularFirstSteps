c745ef69044939ba42eefa0d8e1590dbbab5be6c
qnarik1 <knarik.mh@gmail.com>
2016-11-28 09:32:38 +0400

    adding service

---
+++ js/invoice2.js
@@ -0,0 +1,18 @@
+/**
+ * Created by qnarik on 11/27/16.
+ */
+
+angular.module('invoice2', ['finance2'])
+    .controller('InvoiceController', ['currencyConverter', function InvoiceController(currencyConverter) {
+        this.qty = 1;
+        this.cost = 2;
+        this.inCurr = 'EUR';
+        this.currencies = currencyConverter.currencies;
+
+        this.total = function total(outCurr) {
+            return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
+        };
+        this.pay = function pay() {
+            window.alert('Thanks!');
+        };
+    }]);

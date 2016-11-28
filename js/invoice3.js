1bfcc3213c01619262581817e82ab27015f16bc0
qnarik1 <knarik.mh@gmail.com>
2016-11-28 09:50:36 +0400

    with http appi

---
+++ js/invoice3.js
@@ -0,0 +1,19 @@
+/**
+ * Created by qnarik on 11/27/16.
+ */
+
+angular.module('invoice3', ['finance3'])
+    .controller('InvoiceController', ['currencyConverter', function
+        InvoiceController(currencyConverter) {
+        this.qty = 1;
+        this.cost = 2;
+        this.inCurr = 'EUR';
+        this.currencies = currencyConverter.currencies;
+
+        this.total = function total(outCurr) {
+            return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
+        };
+        this.pay = function pay() {
+            window.alert('Thanks!');
+        };
+    }]);
